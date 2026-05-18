3e51c3fb5b6f3df0a31a805ad80bab472a6575a5
Matheus Calonico <matheusscalonico@gmail.com>
2026-05-18 18:19:37 -0300

    feat: adicionar Footer com 2 colunas (Contato, Disponibilidade)

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import AnimatedGridBackground from "./components/AnimatedGridBackground";
+import Footer from "./components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <AnimatedGridBackground />
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
